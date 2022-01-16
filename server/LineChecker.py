26e3536b275574a94e0769546f4135c2e813268c
MiniaczQ <MiniaczQ@gmail.com>
2022-01-16 17:05:17 +0100

    separate line checker, rename base class

---
+++ server/LineChecker.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from typing import Callable
+import re
+
+
+class LineCheckBase(ABC):
+    @abstractmethod
+    def check(self, string: str) -> None: ...
+
+class LineChecker(LineCheckBase):
+    callback: Callable[[], None]
+    message: str
+
+    def __init__(self, callback: Callable[[], None], message: str) -> None:
+        self.callback = callback
+        self.message = message
+
+    def check(self, string: str) -> None:
+        if self.message in string:
+            self.callback()
+
+class RELineChecker(LineCheckBase):
+    callback: Callable[[], None]
+    pattern: str
+
+    def __init__(self, callback: Callable[[], None], pattern_str: str) -> None:
+        self.callback = callback
+        self.pattern = re.compile(pattern_str)
+
+    def check(self, string: str) -> None:
+        if self.pattern.match(string):
+            self.callback()
--- server/TimerServer.py
+++ server/TimerServer.py
@@ -9,6 +9,8 @@ import socket
 import re
 from typing import Callable
 
+from server.LineChecker import LineCheckBase, LineChecker
+
 
 class TimerClientInstance:
     def __init__(self, parent, c, addr):
@@ -155,33 +157,7 @@ class TimerServer:
         except:
             pass
 
-class LineMatch(ABC):
-    @abstractmethod
-    def check(self, string: str) -> None: ...
 
-class LineChecker(LineMatch):
-    callback: Callable[[], None]
-    message: str
-
-    def __init__(self, callback: Callable[[], None], message: str) -> None:
-        self.callback = callback
-        self.message = message
-
-    def check(self, string: str) -> None:
-        if self.message in string:
-            self.callback()
-
-class RELineChecker(LineMatch):
-    callback: Callable[[], None]
-    pattern: str
-
-    def __init__(self, callback: Callable[[], None], pattern_str: str) -> None:
-        self.callback = callback
-        self.pattern = re.compile(pattern_str)
-
-    def check(self, string: str) -> None:
-        if self.pattern.match(string):
-            self.callback()
 
 
 class LogsTracker:
@@ -192,7 +168,7 @@ class LogsTracker:
         self.running = False
         self.lineCheckers = set([])
 
-    def addChecker(self, lineChecker: LineChecker):
+    def addChecker(self, lineChecker: LineCheckBase):
         self.lineCheckers.add(lineChecker)
 
     def start(self):
